fdf1ba2855781df302acbb6c6703d4630dd1c32f
Abdulrahman <abdulrahman.qabbout@gmail.com>
2021-06-07 16:02:08 +0300

    add slug routing

--- pages/index.js
+++ pages/index.js
@@ -24,16 +24,14 @@ export default function Home({ recipes }) {
         {recipes?.length > 0 &&
           recipes.map((recipe) => (
             <li key={recipe._id} className="recipe-card">
-              <Link href="/">
+              <Link href={`/recipes/${recipe.slug.current}`}>
                 <a>
-                  <img src={urlFor(recipe.mainImage).url()}/>
-                    <span>{recipe.name}</span>
+                  <img src={urlFor(recipe.mainImage).url()} alt={recipe.name}   />
+                  <span>{recipe.name}</span>
                 </a>
               </Link>
             </li>
-          )
-          )
-          }
+          ))}
       </ul>
     </div>
   );
